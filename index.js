17c11a1cf7c5297cc2836f23e771bfc19621b872
AbramsM1A2 <abraham.korez@gmail.com>
2021-05-21 10:03:48 +0200

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require('body-parser');
-
+var cors = require("cors");
 var natalityStatsAPIv2 = require("./src/back/natality-statsAPI/v2");
 
 //--------------------------Server-variables------------------
@@ -12,7 +12,7 @@ var port = process.env.PORT || 10000;
 
 
 //--------------------------Server-usages----------------------
-
+app.use(cors());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(express.json());
